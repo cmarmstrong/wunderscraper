eb54672b7bf61106fbd689c648bb74ae0aeac4d0
cmarmstrong <omni.armstrong@gmail.com>
2017-09-15 10:03:06 -0500

    date on station fnames; wrap main loop in function

--- app.R
+++ app.R
@@ -59,42 +59,47 @@ count <- function(counter) { # schedule and ensure api calls remain within minut
 
 
 counter <- list(count=0, period=0, date=format(Sys.Date(), tz='America/New_York'))
-repeat{
-    s <- sample(coRel $GEOID, 1, replace=TRUE, prob=coRel $COPOP)
-    ## if(any(s %in% OCONUS)) next
-    geolookups <- lapply(zctaRel[zctaRel $GEOID==s, ] $ZCTA5, function(query) {
-        counter <- count(counter)
-        GETjson(wuUrl, WUpath(wuKey, 'geolookup', query, 'json'))
-    })
-    geolookups <- lapply(geolookups, function(zcta) {
-        if(!is.null(zcta $response $error)) return(NA)
-        ## z <- zcta $location $zip
-        with(zcta $location $nearby_weather_stations $pws $station,
-             st_sf(geometry=st_cast(st_sfc(st_multipoint(matrix(c(lon, lat), ncol=2))), 'POINT'),
-                   id=id,
-                   stringsAsFactors=FALSE)) # ,
-                   ## ZCTA5=z)) # zcta is unecessary
-    })
-    geolookups <- do.call(rbind, geolookups[!is.na(geolookups)])
-    geolookups <- geolookups[!duplicated(geolookups $id), ]
-    st_crs(geolookups) <- 4326 ## WU in 4326
-    geolookups <- st_transform(geolookups, st_crs(co))
-    geolookups <- st_intersection(geolookups, co[co $GEOID==s, ])
-    m <- Mclust(st_coordinates(geolookups), modelNames='VII')
-    geolookups $cluster <- as.factor(m $classification)
-    geolookups $grid <- as.factor(unlist(st_intersects(geolookups, st_make_grid(geolookups, 0.01))))
-    geolookups $strata <- with(geolookups, cluster:grid)
-    dirname <- file.path(DATADIR, paste0('geoid', s, '-', as.integer(Sys.time())))
-    dir.create(dirname)
+
+
+main <- function(counter) {
     repeat{
-        stations <- unlist(with(geolookups, tapply(id, strata, sample, 1, simplify=FALSE)))
-        for(station in sample(stations)) { # default sample reorders
+        s <- sample(coRel $GEOID, 1, replace=TRUE, prob=coRel $COPOP)
+        ## if(any(s %in% OCONUS)) next
+        geolookups <- lapply(zctaRel[zctaRel $GEOID==s, ] $ZCTA5, function(query) {
             counter <- count(counter)
-            wuUrn <- WUpath(wuKey, 'conditions', paste('pws', station, sep=':'), 'json')
-            write_json(toJSON(GETjson(wuUrl, wuUrn)), file.path(dirname, paste0(station, '.json')))
+            GETjson(wuUrl, WUpath(wuKey, 'geolookup', query, 'json'))
+        })
+        geolookups <- lapply(geolookups, function(zcta) {
+            if(!is.null(zcta $response $error)) return(NA)
+            ## z <- zcta $location $zip
+            with(zcta $location $nearby_weather_stations $pws $station,
+                 st_sf(geometry=st_cast(st_sfc(st_multipoint(matrix(c(lon, lat), ncol=2))), 'POINT'),
+                       id=id,
+                       stringsAsFactors=FALSE)) # ,
+                       ## ZCTA5=z)) # zcta is unecessary
+        })
+        geolookups <- do.call(rbind, geolookups[!is.na(geolookups)])
+        geolookups <- geolookups[!duplicated(geolookups $id), ]
+        st_crs(geolookups) <- 4326 ## WU in 4326
+        geolookups <- st_transform(geolookups, st_crs(co))
+        geolookups <- st_intersection(geolookups, co[co $GEOID==s, ])
+        m <- Mclust(st_coordinates(geolookups), modelNames='VII')
+        geolookups $cluster <- as.factor(m $classification)
+        geolookups $grid <- as.factor(unlist(st_intersects(geolookups, st_make_grid(geolookups, 0.01))))
+        geolookups $strata <- with(geolookups, cluster:grid)
+        dirname <- file.path(DATADIR, paste0('geoid', s, '-', as.integer(Sys.time())))
+        dir.create(dirname)
+        repeat{
+            stations <- unlist(with(geolookups, tapply(id, strata, sample, 1, simplify=FALSE)))
+            for(station in sample(stations)) { # default sample reorders
+                counter <- count(counter)
+                wuUrn <- WUpath(wuKey, 'conditions', paste('pws', station, sep=':'), 'json')
+                write_json(toJSON(GETjson(wuUrl, wuUrn)),
+                           file.path(dirname, paste0(station, '-', as.integer(Sys.time()), '.json')))
+            }
+            counter $period <- PERIOD # reset sample period
+            if(SAMPLECO) break # sample next county
         }
-        counter $period <- PERIOD # reset sample period
-        if(SAMPLECO) break # sample next county
     }
 }
 
